7f69362a722a5efcedbdb03e739cd22ec7a9d0a7
SillyFreak <clemens.koza@gmx.at>
2013-05-15 19:15:48 +0200

    Added a first replication test: two games inside the same JVM. All data is read from one game, i.e. flow control and output are done from that game. Input actions are created for the other game, serialized to a byte array, deserialized for the other game, and applied without inconsistencies.
    The test case actions do not use IDs that change during the game, and don't support undo.

--- src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
+++ src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
@@ -7,11 +7,18 @@
 package net.slightlymagic.ticTacToe;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 import net.slightlymagic.ticTacToe.action.PlacePieceAction;
 import net.slightlymagic.ticTacToe.action.TTTAction;
 import net.slightlymagic.ticTacToe.sync.Engine;
+import net.slightlymagic.ticTacToe.sync.EntityInputStream;
+import net.slightlymagic.ticTacToe.sync.EntityOutputStream;
 
 
 /**
@@ -23,29 +30,48 @@ import net.slightlymagic.ticTacToe.sync.Engine;
  * @author SillyFreak
  */
 public class TicTacToe {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         try (Scanner sc = new Scanner(System.in);) {
-            Engine eng = new Engine();
+            Engine eng1 = new Engine();
+            Engine eng2 = new Engine();
             
             
-            TTTGame game = new TTTGame(eng);
-            while(game.isGameRunning()) {
+            TTTGame game1 = new TTTGame(eng1);
+            TTTGame game2 = new TTTGame(eng2);
+            while(game2.isGameRunning()) {
                 int x = sc.nextInt(), y = sc.nextInt();
                 
-                TTTAction action = new PlacePieceAction(game, game.getNextPlayer(), x, y);
-                action.apply();
+                TTTAction action1 = new PlacePieceAction(game1, game1.getNextPlayer(), x, y);
+                action1.apply();
+                
+                byte[] buf;
+                try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ObjectOutputStream oos = new EntityOutputStream(eng1, baos);) {
+                    
+                    oos.writeObject(action1);
+                    oos.flush();
+                    buf = baos.toByteArray();
+                }
+                
+                try (ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+                        ObjectInputStream ois = new EntityInputStream(eng2, bais)) {
+                    
+                    TTTAction action2 = (TTTAction) ois.readObject();
+                    action2.apply();
+                }
+                
                 
                 System.out.printf("%s%s%s|%n%s%s%s|%n%s%s%s|%n", //
-                        p(game, 0, 0), p(game, 1, 0), p(game, 2, 0), //
-                        p(game, 0, 1), p(game, 1, 1), p(game, 2, 1), //
-                        p(game, 0, 2), p(game, 1, 2), p(game, 2, 2));
+                        p(game2, 0, 0), p(game2, 1, 0), p(game2, 2, 0), //
+                        p(game2, 0, 1), p(game2, 1, 1), p(game2, 2, 1), //
+                        p(game2, 0, 2), p(game2, 1, 2), p(game2, 2, 2));
             }
-            System.out.println("===# " + game.getWinner().getId());
+            System.out.println("===# " + game2.getWinner().getPlayerId());
         }
     }
     
     private static String p(TTTGame game, int x, int y) {
         TTTPiece piece = game.getPiece(x, y);
-        return piece == null? " ":"" + piece.getOwner().getId();
+        return piece == null? " ":"" + piece.getOwner().getPlayerId();
     }
 }
