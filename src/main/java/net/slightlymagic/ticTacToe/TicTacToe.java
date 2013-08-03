8cd762ba0a86f8364f9637f980d44dc25181a1ef
SillyFreak <clemens.koza@gmx.at>
2013-08-03 12:04:50 +0200

    Changed TicTacToe from a game & observer to a player vs player setup: the engines alternate in executing actions

--- src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
+++ src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
@@ -35,22 +35,28 @@ public class TicTacToe {
             
             host1.newGame();
             host2.connectToGame(host1);
-            TTTGame game1 = host1.getGame(), game2 = host2.getGame();
             
-            while(game2.isGameRunning()) {
-                int x = sc.nextInt(), y = sc.nextInt();
-                
-                Action action = new PlacePieceAction(host1.getEngine(), game1, game1.getNextPlayer(), x, y);
-                host1.getBranchManager().execute(action);
-                
-                host2.synchronize(host1);
-                
-                System.out.printf("%s%s%s|%n%s%s%s|%n%s%s%s|%n", //
-                        p(game2, 0, 0), p(game2, 1, 0), p(game2, 2, 0), //
-                        p(game2, 0, 1), p(game2, 1, 1), p(game2, 2, 1), //
-                        p(game2, 0, 2), p(game2, 1, 2), p(game2, 2, 2));
+            for(int i = 0;; i++) {
+                if(i % 2 == 0) {
+                    makeMove(host1, sc);
+                    host2.synchronize(host1);
+                    
+                    if(!host2.getGame().isGameRunning()) {
+                        print(host2.getGame());
+                        System.out.println("===# " + host2.getGame().getWinner().getPlayerId());
+                        break;
+                    }
+                } else {
+                    makeMove(host2, sc);
+                    host1.synchronize(host2);
+                    
+                    if(!host1.getGame().isGameRunning()) {
+                        print(host1.getGame());
+                        System.out.println("===# " + host1.getGame().getWinner().getPlayerId());
+                        break;
+                    }
+                }
             }
-            System.out.println("===# " + game2.getWinner().getPlayerId());
             
             System.out.println(host1.getEngine());
             for(State s = host1.getEngine().getHead(); s != null; s = s.getParent())
@@ -60,6 +66,23 @@ public class TicTacToe {
             for(State s = host2.getEngine().getHead(); s != null; s = s.getParent())
                 System.out.println(s);
         }
+    }
+    
+    private static void makeMove(Host current, Scanner sc) {
+        TTTGame game = current.getGame();
+        print(game);
+        
+        int x = sc.nextInt(), y = sc.nextInt();
+        
+        Action action = new PlacePieceAction(current.getEngine(), game, game.getNextPlayer(), x, y);
+        current.getBranchManager().execute(action);
+    }
+    
+    private static void print(TTTGame game) {
+        System.out.printf("%s%s%s|%n%s%s%s|%n%s%s%s|%n", //
+                p(game, 0, 0), p(game, 1, 0), p(game, 2, 0), //
+                p(game, 0, 1), p(game, 1, 1), p(game, 2, 1), //
+                p(game, 0, 2), p(game, 1, 2), p(game, 2, 2));
     }
     
     private static void config(Host host) {
