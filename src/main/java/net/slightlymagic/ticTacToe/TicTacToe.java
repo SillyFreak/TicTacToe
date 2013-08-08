2f2768d1e142bc8a690c9937d93963f4276b71f7
SillyFreak <clemens.koza@gmx.at>
2013-08-08 22:09:30 +0200

    Changed to a graphical game, works even better because of event based GUI

--- src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
+++ src/main/java/net/slightlymagic/ticTacToe/TicTacToe.java
@@ -7,15 +7,14 @@
 package net.slightlymagic.ticTacToe;
 
 
-import java.util.Scanner;
+import static java.lang.String.*;
+
+import javax.swing.JFrame;
 
 import net.slightlymagic.ticTacToe.action.NewGameAction;
 import net.slightlymagic.ticTacToe.action.PlacePieceAction;
-import at.pria.koza.harmonic.Action;
 import at.pria.koza.harmonic.BranchManager;
 import at.pria.koza.harmonic.Engine;
-import at.pria.koza.harmonic.HeadListener;
-import at.pria.koza.harmonic.State;
 import at.pria.koza.polybuf.PolybufConfig;
 
 
@@ -29,32 +28,15 @@ import at.pria.koza.polybuf.PolybufConfig;
  */
 public class TicTacToe {
     public static void main(String[] args) throws Exception {
-        try (Scanner sc = new Scanner(System.in);) {
-            Host host = config("ticTacToe");
-            
-            System.out.printf("I am %08X%n", host.getEngine().getId());
-            do {
-                System.out.println("enter 'start' to start a game, or press enter when your partner has started the game");
-                String line = sc.nextLine();
-                if("start".equalsIgnoreCase(line)) {
-                    host.newGame();
-                    host.publish(0, BranchManager.BRANCH_DEFAULT);
-                }
-            } while(host.getGame() == null);
-            
-            for(;;) {
-                makeMove(host, sc);
-                host.publish(0, BranchManager.BRANCH_DEFAULT);
-            }
-        }
-    }
-    
-    private static void makeMove(Host host, Scanner sc) {
-        TTTGame game = host.getGame();
-        int x = sc.nextInt(), y = sc.nextInt();
+        Host host = config("ticTacToe");
         
-        Action action = new PlacePieceAction(host.getEngine(), game, game.getNextPlayer(), x, y);
-        host.getBranchManager().execute(action);
+        JFrame jf = new JFrame(format("%08X", host.getEngine().getId()));
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        jf.add(new TicTacToePanel(host));
+        
+        jf.pack();
+        jf.setVisible(true);
     }
     
     private static Host config(String cluster) throws Exception {
@@ -68,27 +50,6 @@ public class TicTacToe {
         PlacePieceAction.configure(config, engine);
         NewGameAction.configure(config, engine);
         
-        engine.addHeadListener(new HeadListener() {
-            @Override
-            public void headMoved(State prevHead, State newHead) {
-                print(host.getGame());
-                if(!host.getGame().isGameRunning()) {
-                    System.out.println("===# " + host.getGame().getWinner().getPlayerId());
-                }
-            }
-        });
         return host;
-    }
-    
-    private static void print(TTTGame game) {
-        System.out.printf("%s%s%s|%n%s%s%s|%n%s%s%s|%n", //
-                p(game, 0, 0), p(game, 1, 0), p(game, 2, 0), //
-                p(game, 0, 1), p(game, 1, 1), p(game, 2, 1), //
-                p(game, 0, 2), p(game, 1, 2), p(game, 2, 2));
-    }
-    
-    private static String p(TTTGame game, int x, int y) {
-        TTTPiece piece = game.getPiece(x, y);
-        return piece == null? " ":"" + piece.getOwner().getPlayerId();
     }
 }
---
+++ src/main/java/net/slightlymagic/ticTacToe/TicTacToePanel.java
@@ -0,0 +1,141 @@
+/**
+ * TicTacToePanel.java
+ * 
+ * Created on 08.08.2013
+ */
+
+package net.slightlymagic.ticTacToe;
+
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.CompoundBorder;
+
+import net.slightlymagic.ticTacToe.action.PlacePieceAction;
+import at.pria.koza.harmonic.Action;
+import at.pria.koza.harmonic.BranchManager;
+import at.pria.koza.harmonic.HeadListener;
+import at.pria.koza.harmonic.State;
+
+
+/**
+ * <p>
+ * {@code TicTacToePanel}
+ * </p>
+ * 
+ * @version V1.0 08.08.2013
+ * @author SillyFreak
+ */
+public class TicTacToePanel extends JPanel {
+    private static final long serialVersionUID = -4160262603196922197L;
+    
+    private final Host        host;
+    
+    private final JButton     start;
+    private final JButton[][] buttons;
+    
+    public TicTacToePanel(Host host) {
+        super(new BorderLayout());
+        
+        this.host = host;
+        buttons = new JButton[3][3];
+        
+        JPanel field = new JPanel(new GridLayout(3, 3));
+        Dimension dim = new Dimension(60, 60);
+        for(int i = 0; i < buttons.length; i++) {
+            for(int j = 0; j < buttons[i].length; j++) {
+                JButton b = new JButton(new PlaceAction(i, j));
+                b.setBorder(((CompoundBorder) b.getBorder()).getOutsideBorder());
+                b.setPreferredSize(dim);
+                buttons[i][j] = b;
+                field.add(b);
+            }
+        }
+        add(field);
+        
+        start = new JButton(new StartAction());
+        add(start, BorderLayout.NORTH);
+        
+        update();
+        host.getEngine().addHeadListener(new UpdateListener());
+    }
+    
+    public void update() {
+        for(int i = 0; i < buttons.length; i++) {
+            for(int j = 0; j < buttons[i].length; j++) {
+                updateButton(i, j);
+            }
+        }
+    }
+    
+    private void updateButton(int i, int j) {
+        JButton b = buttons[i][j];
+        TTTGame game = host.getGame();
+        start.setEnabled(game == null);
+        if(game == null) {
+            b.setEnabled(false);
+            b.setText("");
+        } else {
+            TTTPiece p = game.getPiece(i, j);
+            b.setEnabled(p == null && game.isGameRunning());
+            switch(p == null? -1:p.getOwner().getPlayerId()) {
+                case -1:
+                    b.setText("");
+                break;
+                case 0:
+                    b.setText("X");
+                break;
+                case 1:
+                    b.setText("O");
+                break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+    
+    private final class PlaceAction extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+        private int               i, j;
+        
+        public PlaceAction(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            TTTGame game = host.getGame();
+            Action action = new PlacePieceAction(host.getEngine(), game, game.getNextPlayer(), i, j);
+            host.getBranchManager().execute(action);
+            host.publish(0, BranchManager.BRANCH_DEFAULT);
+        }
+    }
+    
+    private final class StartAction extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+        
+        public StartAction() {
+            super("New Game");
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            host.newGame();
+            host.publish(0, BranchManager.BRANCH_DEFAULT);
+        }
+    }
+    
+    private final class UpdateListener implements HeadListener {
+        @Override
+        public void headMoved(State prevHead, State newHead) {
+            update();
+        }
+    }
+}
