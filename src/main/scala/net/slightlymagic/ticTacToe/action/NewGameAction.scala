dad90d9629a0440b7d0ce3d5d980fea8ff77d05e
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-18 00:44:33 +0100

    scala-ification

---
+++ src/main/scala/net/slightlymagic/ticTacToe/action/NewGameAction.scala
@@ -0,0 +1,63 @@
+/**
+ * NewGameAction.scala
+ *
+ * Created on 28.07.2013
+ */
+
+package net.slightlymagic.ticTacToe.action
+
+import at.pria.koza.harmonic.Action
+import at.pria.koza.harmonic.Engine
+import at.pria.koza.harmonic.IOFactory
+import at.pria.koza.polybuf.PolybufException
+import at.pria.koza.polybuf.PolybufIO
+import at.pria.koza.polybuf.PolybufInput
+import at.pria.koza.polybuf.PolybufOutput
+import at.pria.koza.polybuf.PolybufSerializable
+import at.pria.koza.polybuf.proto.Polybuf.Obj
+
+import com.google.protobuf.GeneratedMessage.GeneratedExtension
+
+import net.slightlymagic.ticTacToe.TTTGame
+import net.slightlymagic.ticTacToe.proto.TTTP.NewGameActionP
+
+/**
+ * <p>
+ * {@code NewGameAction}
+ * </p>
+ *
+ * @version V0.0 28.07.2013
+ * @author SillyFreak
+ */
+object NewGameAction extends IOFactory[NewGameAction] {
+  val FIELD = NewGameActionP.NEW_GAME_ACTION_FIELD_NUMBER
+  val EXTENSION = NewGameActionP.newGameAction
+
+  def getIO(engine: Engine): PolybufIO[NewGameAction] = new IO(engine)
+
+  private class IO(engine: Engine) extends PolybufIO[NewGameAction] {
+    override def extension: GeneratedExtension[Obj, NewGameActionP] = EXTENSION
+
+    @throws[PolybufException]
+    override def serialize(out: PolybufOutput, instance: NewGameAction, obj: Obj.Builder): Unit = {
+      obj.setExtension(extension, NewGameActionP.newBuilder().build());
+    }
+
+    @throws[PolybufException]
+    override def initialize(in: PolybufInput, obj: Obj): NewGameAction = {
+      //val p = obj.getExtension(extension)
+      return new NewGameAction(engine)
+    }
+  }
+}
+
+class NewGameAction(engine: Engine) extends Action(engine) with PolybufSerializable {
+  //PolybufSerializable
+  def typeId: Int = NewGameAction.FIELD
+
+  private var _game: TTTGame = _
+  def game: TTTGame = _game
+
+  override protected[this] def apply0(): Unit =
+    _game = new TTTGame(engine)
+}
---
+++ src/main/scala/net/slightlymagic/ticTacToe/action/PlacePieceAction.scala
@@ -0,0 +1,72 @@
+/**
+ * PlacePieceAction.scala
+ *
+ * Created on 14.05.2013
+ */
+
+package net.slightlymagic.ticTacToe.action
+
+import at.pria.koza.harmonic.Action
+import at.pria.koza.harmonic.Engine
+import at.pria.koza.harmonic.IOFactory
+import at.pria.koza.polybuf.PolybufException
+import at.pria.koza.polybuf.PolybufIO
+import at.pria.koza.polybuf.PolybufInput
+import at.pria.koza.polybuf.PolybufOutput
+import at.pria.koza.polybuf.PolybufSerializable
+import at.pria.koza.polybuf.proto.Polybuf.Obj
+
+import com.google.protobuf.GeneratedMessage.GeneratedExtension;
+
+import net.slightlymagic.ticTacToe.TTTGame
+import net.slightlymagic.ticTacToe.TTTPlayer
+import net.slightlymagic.ticTacToe.proto.TTTP.PlacePieceActionP
+
+/**
+ * <p>
+ * The class PlacePieceAction.
+ * </p>
+ *
+ * @version V0.0 14.05.2013
+ * @author SillyFreak
+ */
+object PlacePieceAction extends IOFactory[PlacePieceAction] {
+  val FIELD = PlacePieceActionP.PLACE_PIECE_ACTION_FIELD_NUMBER
+  val EXTENSION = PlacePieceActionP.placePieceAction
+
+  def getIO(engine: Engine): PolybufIO[PlacePieceAction] = new IO(engine)
+
+  private class IO(engine: Engine) extends PolybufIO[PlacePieceAction] {
+    override def extension: GeneratedExtension[Obj, PlacePieceActionP] = EXTENSION
+
+    @throws[PolybufException]
+    override def serialize(out: PolybufOutput, instance: PlacePieceAction, obj: Obj.Builder): Unit = {
+      val b = PlacePieceActionP.newBuilder()
+      b.setGame(instance.game.id)
+      b.setPlayer(instance.player.id)
+      b.setX(instance.x)
+      b.setY(instance.y)
+
+      obj.setExtension(extension, b.build())
+    }
+
+    @throws[PolybufException]
+    override def initialize(in: PolybufInput, obj: Obj): PlacePieceAction = {
+      val p = obj.getExtension(extension)
+      val game = engine.entity(p.getGame()).asInstanceOf[TTTGame]
+      val player = engine.entity(p.getPlayer()).asInstanceOf[TTTPlayer]
+      val x = p.getX()
+      val y = p.getY()
+
+      return new PlacePieceAction(engine, game, player, x, y)
+    }
+  }
+}
+
+class PlacePieceAction(engine: Engine, val game: TTTGame, val player: TTTPlayer, val x: Int, val y: Int) extends Action(engine) with PolybufSerializable {
+  //PolybufSerializable
+  def typeId: Int = PlacePieceAction.FIELD
+
+  override protected[this] def apply0(): Unit =
+    game.placePiece(player, x, y)
+}
