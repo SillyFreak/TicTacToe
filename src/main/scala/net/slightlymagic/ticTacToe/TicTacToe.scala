4590b7d3dfb2d1e888bdcacb4a6e41cb11473c07
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-18 17:37:33 +0100

    scala-ification

---
+++ src/main/scala/net/slightlymagic/ticTacToe/TicTacToe.scala
@@ -0,0 +1,52 @@
+/**
+ * TicTacToe.scala
+ *
+ * Created on 14.05.2013
+ */
+
+package net.slightlymagic.ticTacToe
+
+import java.lang.String._
+
+import javax.swing.JFrame
+
+import net.slightlymagic.ticTacToe.action.NewGameAction
+import net.slightlymagic.ticTacToe.action.PlacePieceAction
+
+/**
+ * <p>
+ * The class TicTacToe.
+ * </p>
+ *
+ * @version V0.0 14.05.2013
+ * @author SillyFreak
+ */
+object TicTacToe {
+  @throws[Exception]
+  def main(args: Array[String]): Unit = {
+    val host = config("ticTacToe")
+
+    val jf = new JFrame("%08X".format(host.engine.id))
+    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+    jf.add(new TicTacToePanel(host))
+
+    jf.pack()
+    jf.setVisible(true)
+  }
+
+  @throws[Exception]
+  def config(cluster: String): Host = {
+    val host = new Host(cluster)
+
+    val mgr = host.mgr
+    val engine = mgr.engine
+    val config = engine.config
+
+    mgr.configure(config)
+    engine.addIO(PlacePieceAction)
+    engine.addIO(NewGameAction)
+
+    host
+  }
+}
