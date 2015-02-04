6b3b9604a2f897b73868f21043887701cbabde88
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-02-04 23:06:36 +0100

    extract createPanel that adds a HarmonicViewer

--- src/main/scala/net/slightlymagic/ticTacToe/TicTacToe.scala
+++ src/main/scala/net/slightlymagic/ticTacToe/TicTacToe.scala
@@ -6,7 +6,13 @@
 
 package net.slightlymagic.ticTacToe
 
+import at.pria.koza.harmonic.viewer.HarmonicViewer
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+
 import javax.swing.JFrame
+import javax.swing.JPanel
 
 import at.pria.koza.harmonic.StateNode
 
@@ -33,12 +39,27 @@ object TicTacToe {
       host
     }
 
+    def createPanel(host: Host) = {
+      val jp = new JPanel(new BorderLayout())
+      jp.add({
+        new TicTacToePanel(host)
+      }, BorderLayout.WEST)
+      jp.add {
+        val viewer = new HarmonicViewer()
+        viewer.setMinimumSize(new Dimension(300, 0))
+        viewer.setPreferredSize(new Dimension(300, 0))
+        viewer.listenTo(host.engine)
+        viewer
+      }
+      jp
+    }
+
     val host = createHost()
 
     val jf = new JFrame("%08X".format(host.engine.id))
     jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
-    jf.add(new TicTacToePanel(host))
+    jf.add(createPanel(host))
 
     jf.pack()
     jf.setVisible(true)
